ed75a892d83454624f2083d3ecb4732c3b10a1f0
Conor Haughney <cphaughney99@gmail.com>
2025-03-10 11:50:21 +0000

    Changed URLs & started some pages

--- RecipeSite/Recipes/views.py
+++ RecipeSite/Recipes/views.py
@@ -3,4 +3,20 @@ from django.http import HttpResponse
 
 def test(request):
     context_dict = {}
-    return render(request, 'Recipes/base.html', context=context_dict)
+    return render(request, 'Recipes/home.html', context=context_dict)
+
+def home(request):
+    context_dict = {}
+    return render(request, 'Recipes/home.html', context=context_dict)
+
+def login(request):
+    context_dict = {}
+    return render(request, 'Recipes/login.html', context=context_dict)
+
+def recipes(request):
+    context_dict = {}
+    return render(request, 'Recipes/recipes.html', context=context_dict)
+
+def search(request):
+    context_dict = {}
+    return render(request, 'Recipes/search_recipe.html', context=context_dict)
