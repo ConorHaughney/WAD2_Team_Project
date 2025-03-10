c0e4d855062d0725e87b2e391e9ad4dc8db09e29
Ben Clydesdale <2768789C@student.gla.ac.uk>
2025-03-10 23:11:42 +0000

    login page setup, all links fixed with additional pages created

--- RecipeSite/Recipes/urls.py
+++ RecipeSite/Recipes/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('recipes/', views.recipes, name='recipes'),
     path('search/', views.search, name='search'),
+    path('register/', views.create_account, name='create_account'),
+    path('recipes/most_popular/', views.most_popular, name='most_popular'),
+    path('recipes/your-favourites/', views.favourites, name='favourites'),
+    path('recipes/add-your-own/', views.add_recipe, name='add_recipe'),
+    path('recipes/<slug:recipe_name_slug>/',
+         views.show_recipe, name='show_recipe'),
+    
 ]
--- RecipeSite/Recipes/views.py
+++ RecipeSite/Recipes/views.py
@@ -1,22 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Recipes.models import Recipe
+
+
+
 def test(request):
     context_dict = {}
     return render(request, 'Recipes/home.html', context=context_dict)
+
+
 
 def home(request):
     context_dict = {}
     return render(request, 'Recipes/home.html', context=context_dict)
 
+
+
 def login(request):
     context_dict = {}
     return render(request, 'Recipes/login.html', context=context_dict)
+
+
 
 def recipes(request):
     context_dict = {}
     return render(request, 'Recipes/recipes.html', context=context_dict)
 
+
+
 def search(request):
     context_dict = {}
     return render(request, 'Recipes/search_recipe.html', context=context_dict)
+
+
+
+def show_recipe(request, recipe_name_slug):
+    context_dict = {}
+
+    try:
+        recipe = Recipe.objects.get(slug=recipe_name_slug)
+
+        context_dict['recipe'] = recipe
+    except Recipe.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None    
+    return render(request, 'Recipes/show_recipe.html', context=context_dict)
+
+
+
+def add_recipe(request):
+    context_dict = {}
+    return render(request, 'Recipes/add_recipe.html', context=context_dict)
+
+
+
+def create_account(request):
+    context_dict = {}
+    return render(request, 'Recipes/create_account.html', context=context_dict)
+
+def favourites(request):
+    context_dict = {}
+    return render(request, 'Recipes/your_favourites.html', context=context_dict)
+
+def most_popular(request):
+    context_dict = {}
+    return render(request, 'Recipes/most_popular.html', context=context_dict)
