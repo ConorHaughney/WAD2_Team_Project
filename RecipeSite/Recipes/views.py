21414eaf33b7171cfd026fdd6d143c39767a4062
Ben Clydesdale <2768789C@student.gla.ac.uk>
2025-03-11 10:03:52 +0000

    login and logout functionality working

--- RecipeSite/Recipes/views.py
+++ RecipeSite/Recipes/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 from Recipes.models import Recipe
 
@@ -17,9 +22,9 @@ def home(request):
 
 
 
-def login(request):
-    context_dict = {}
-    return render(request, 'Recipes/login.html', context=context_dict)
+# def login(request):
+#     context_dict = {}
+#     return render(request, 'Recipes/login.html', context=context_dict)
 
 
 
@@ -66,3 +71,29 @@ def favourites(request):
 def most_popular(request):
     context_dict = {}
     return render(request, 'Recipes/most_popular.html', context=context_dict)
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('Recipes:home'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+        
+    else:
+        return render(request, 'Recipes/login.html')
+    
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return redirect(reverse('Recipes:home'))
