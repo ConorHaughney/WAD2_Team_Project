ff77a8c6e2b33d317349b08c59bb89f8d07719d4
Bin <2960789d@student.gla.ac.uk>
2025-03-12 21:35:46 +0000

    Implementation form

--- RecipeSite/Recipes/forms.py
+++ RecipeSite/Recipes/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from Recipes.models import UserProfile
+from Recipes.models import UserProfile, Recipe, Reviews
 
+# User registration form
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
@@ -9,7 +10,23 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password')
 
+# User profile form
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('picture')
+        fields = ('picture',)
+
+# Add Recipe form
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['recipe_name', 'cuisine', 'difficulty', 'time_taken', 'instructions', 'portion', 'picture']
+
+# Leave a comment form
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        fields = ['rating', 'comment']
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+        }
