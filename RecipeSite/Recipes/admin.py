ab0477da34307a8d927943af115cf219b94ac090
yuhang-feng404 <160128050+yuhang-feng404@users.noreply.github.com>
2025-03-13 15:35:17 +0000

    Updated admin panel in admin.py

--- RecipeSite/Recipes/admin.py
+++ RecipeSite/Recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile, Cuisine, Difficulty, Recipe
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'picture')
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Difficulty)
+class DifficultyAdmin(admin.ModelAdmin):
+    list_display = ('difficulty',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe_name', 'cuisine', 'difficulty')
+    search_fields = ('recipe_name',)
+    list_filter = ('cuisine', 'difficulty')
