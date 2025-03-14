def3270e1ca605bc33fabaeef9586a07163cddd8
Ben Clydesdale <2768789C@student.gla.ac.uk>
2025-03-14 13:38:19 +0000

    fixed populateDB.py to correctly populate the database, amended admin.py to include recipe ingredients

--- RecipeSite/Recipes/admin.py
+++ RecipeSite/Recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Cuisine, Difficulty, Recipe
+from .models import UserProfile, Cuisine, Difficulty, Recipe, Ingredients
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
@@ -20,3 +20,8 @@ class RecipeAdmin(admin.ModelAdmin):
     list_filter = ('cuisine', 'difficulty')
     list_per_page = 20
     ordering = ('recipe_name',)
+
+@admin.register(Ingredients)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient_name', 'quantity')
+    ordering = ('recipe',)
