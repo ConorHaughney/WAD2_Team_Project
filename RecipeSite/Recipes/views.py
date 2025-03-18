c64403669b5f4cb17a19ad4ecd97051829cf54fc
Conor Haughney <cphaughney99@gmail.com>
2025-03-18 22:38:09 +0000

    Recipes and Most Popular

--- RecipeSite/Recipes/views.py
+++ RecipeSite/Recipes/views.py
@@ -148,6 +148,7 @@ def user_logout(request):
 # Most popular recipes page
 def most_popular(request):
     recipes = Recipe.objects.annotate(avg_rating=Avg('reviews__rating')).order_by('-avg_rating')[:10]
+            
     context_dict = {'Recipes': recipes}
     return render(request, 'Recipes/most_popular.html', context_dict)
 
