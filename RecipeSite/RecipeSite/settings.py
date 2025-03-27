9e23d7084307e5a6fd11a281997588c4765dfea5
Ben Clydesdale <2768789C@student.gla.ac.uk>
2025-03-27 12:09:39 +0000

    allow pythonanywhere to host

--- RecipeSite/RecipeSite/settings.py
+++ RecipeSite/RecipeSite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '*2g^8bm(*#apr+ob3sn_0mcv^!3d3ujl(pg%8oy%&@dg*j$v_*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['finerstorm30.pythonanywhere.com']
 
 
 # Application definition
